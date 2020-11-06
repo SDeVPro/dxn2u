d51fde292bf43fe9fde59c48589c97965d5eefbd
SDeVPro <sardor5676626@gmail.com>
2020-11-06 18:31:43 +0500

    fix

--- dxn/settings.py
+++ dxn/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -58,6 +61,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'dxn.urls'
+
 
 TEMPLATES = [
     {
@@ -126,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     'home/static/',
@@ -154,4 +158,5 @@ CKEDITOR_CONFIGS = {
     },
 }
 
+django_heroku.settings(locals())
 ###################################
